f65b36a1807715ac1ff6d3ae2cdf97bd721be950
Scott Lovegrove <scottisafool@live.co.uk>
2015-07-31 11:13:37 +0100

    Added PersonalisationService sample

---
+++ PersonalisationService/PersonalisationService/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Cimbalino.Toolkit.Services;
+
+namespace PersonalisationService
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            var personalisation = new PersonalizationService();
+            if (personalisation.IsSupported)
+            {
+                await personalisation.SetWallpaperImageAsync("ms-appx:///Assets/Wallpaper.jpg", true);
+            }
+        }
+    }
+}
