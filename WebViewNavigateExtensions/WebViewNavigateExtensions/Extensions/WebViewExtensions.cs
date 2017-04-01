28e85fde8e7fa9e9354b4cda3149cedd03fdcccf
Scott Lovegrove <scottisafool@live.co.uk>
2017-04-01 13:48:49 +0100

    Added WebViewExtensions project

---
+++ WebViewNavigateExtensions/WebViewNavigateExtensions/Extensions/WebViewExtensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+
+namespace WebViewNavigateExtensions.Extensions
+{
+    public static class WebViewExtensions
+    {
+        public static Task<WebViewNavigationResult> NavigateAsync(this WebView webView, Uri uri, HttpMethod method = null)
+        {
+            var httpMethod = method ?? HttpMethod.Get;
+
+            TypedEventHandler<WebView, WebViewNavigationCompletedEventArgs> completedHandler = null;
+            WebViewNavigationFailedEventHandler failedHandler = null;
+            var tcs = new TaskCompletionSource<WebViewNavigationResult>();
+
+            completedHandler = (sender, args) =>
+            {
+                webView.NavigationCompleted -= completedHandler;
+                var status = args.IsSuccess ? 200 : (int) args.WebErrorStatus;
+                tcs.SetResult(new WebViewNavigationResult(args.Uri, status));
+            };
+
+            failedHandler = (sender, args) =>
+            {
+                webView.NavigationFailed -= failedHandler;
+                tcs.SetResult(new WebViewNavigationResult(args.Uri, (int)args.WebErrorStatus));
+            };
+
+            webView.NavigationCompleted += completedHandler;
+            webView.NavigationFailed += failedHandler;
+
+            var requestMessage = new HttpRequestMessage(httpMethod, uri);
+
+            webView.NavigateWithHttpRequestMessage(requestMessage);
+
+            return tcs.Task;
+        }
+
+        public static Task<WebViewNavigationResult> NavigateAsync(this WebView webView, string url, HttpMethod method = null)
+        {
+            return webView.NavigateAsync(new Uri(url), method);
+        }
+    }
+
+    public class WebViewNavigationResult
+    {
+        public Uri Uri { get; }
+
+        public int StatusCode { get; }
+
+        public bool IsSuccess => StatusCode >= 200 && StatusCode < 300;
+
+        public WebViewNavigationResult(Uri uri, int statusCode)
+        {
+            Uri = uri;
+            StatusCode = statusCode;
+        }
+    }
+}
---
+++ WebViewNavigateExtensions/WebViewNavigateExtensions/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using WebViewNavigateExtensions.Extensions;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace WebViewNavigateExtensions
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void NavigateTo_OnClick(object sender, RoutedEventArgs e)
+        {
+            var result = await MainWebView.NavigateAsync("http://microsoft.com");
+
+            var dialog = new MessageDialog($"Navigation was successful? {result.IsSuccess}");
+            await dialog.ShowAsync();
+        }
+    }
+}
